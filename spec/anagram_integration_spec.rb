9a94f50653573fb2014fa39f6d2e9fd5afe4ae69
Alyssa Horrocks & Michael Bunker <student@epicodus.com>
2015-08-06 16:39:55 -0700

    add index.erb page for Sinatra

--- spec/anagram_integration_spec.rb
+++ spec/anagram_integration_spec.rb
@@ -8,7 +8,7 @@ describe('the anagram path', {:type => :feature}) do
     visit('/')
     fill_in('anagram', :with => 'cat')
     fill_in('word2', :with => 'act')
-    fill_in('word3' :with => 'tac')
+    fill_in('word3', :with => 'tac')
     click_button('Send')
     expect(page).to have_content('They are both Anagrams!')
   end

65d1fe704a7277bd6320d8d6f8832a6cc337ebd7
Alyssa Horrocks & Michael Bunker <student@epicodus.com>
2015-08-06 15:43:35 -0700

    Allow two words to be input with initial test word.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,6 +1,10 @@
 class String
-  define_method(:anagram) do
-    anagram_word = self.each_char()
-    anagram_word.sort()
+  define_method(:anagram) do |comparison2, comparison3|
+    anagram_word = self.each_char().sort()
+    word_input2 = comparison2.each_char().sort()
+    word_input3 = comparison3.each_char().sort()
+
+    anagram_word
+    word_input2
   end
 end

a6043fa2463a3065a26f11b1817227fc54278ab2
Alyssa Horrocks & Michael Bunker <student@epicodus.com>
2015-08-06 15:36:00 -0700

    add second spec to compare single word to another

--- spec/anagram_spec.rb
+++ spec/anagram_spec.rb
@@ -5,4 +5,9 @@ describe('String#anagram') do
   it('Compares one letter to another') do
     expect(("a").anagram()).to(eq(["a"]))
   end
+
+describe('String#anagram') do
+  it('Compares a single word to another') do
+    expect(("alyssa").anagram()).to(eq(["a","a","l","s","s","y"]))
+  end
 end
